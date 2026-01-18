2606ca692107d6d702caaa28830f21335aa6f08a
Metody <metody.p.petrov@gmail.com>
2026-01-18 20:36:17 +0200

    final

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -9,7 +9,54 @@ const int MAX_SIZE = 10;
 const int MAX_NAME = 100;
 const int LEADERBOARD_SIZE = 5;
 
-void clearBoard(int board[MAX_SIZE][MAX_SIZE], int size) {
+void myStrcpy(char destination[], const char source[]) {
+	int i = 0;
+
+	while (source[i] != '\0') {
+		destination[i] = source[i];
+		i++;
+	}
+
+	destination[i] = '\0';
+}
+int myStrlen(const char str[]) {
+	int length = 0;
+
+	while (str[length] != '\0') {
+		length++;
+	}
+
+	return length;
+}
+void myStrcat(char destination[], const char source[]) {
+	int destLength = myStrlen(destination);
+	int i = 0;
+
+	while (source[i] != '\0') {
+		destination[destLength + i] = source[i];
+		i++;
+	}
+
+	destination[destLength + i] = '\0';
+}
+char digitToChar(const int number) {
+	return char(number + '0');
+}
+void buildLeaderboardFilename(char filename[], int const size) {
+	myStrcpy(filename, "leaderboard_");
+
+	char digit[2];
+	digit[0] = digitToChar(size);
+	digit[1] = '\0';
+
+	myStrcat(filename, digit);
+	myStrcat(filename, "x");
+	myStrcat(filename, digit);
+	myStrcat(filename, ".txt");
+}
+
+
+void clearBoard(int board[MAX_SIZE][MAX_SIZE], const int size) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			board[i][j] = 0;
@@ -17,7 +64,7 @@ void clearBoard(int board[MAX_SIZE][MAX_SIZE], int size) {
 	}
 }
 
-void printBoard(int board[MAX_SIZE][MAX_SIZE], int size, int score) {
+void printBoard(int board[MAX_SIZE][MAX_SIZE], const int size, const int score) {
 	cout << "\nScore: " << score << "\n\n";
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
@@ -27,7 +74,7 @@ void printBoard(int board[MAX_SIZE][MAX_SIZE], int size, int score) {
 	}
 }
 
-void addRandomTile(int board[MAX_SIZE][MAX_SIZE], int size) {
+void addRandomTile(int board[MAX_SIZE][MAX_SIZE], const int size) {
 	int emptyRow[MAX_SIZE * MAX_SIZE];
 	int emptyCol[MAX_SIZE * MAX_SIZE];
 	int count = 0;
@@ -52,7 +99,7 @@ void addRandomTile(int board[MAX_SIZE][MAX_SIZE], int size) {
 	board[emptyRow[index]][emptyCol[index]] = value;
 }
 
-bool hasMoves(int board[MAX_SIZE][MAX_SIZE], int size) {
+bool hasMoves(int board[MAX_SIZE][MAX_SIZE], const int size) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			if (board[i][j] == 0) {
@@ -69,23 +116,173 @@ bool hasMoves(int board[MAX_SIZE][MAX_SIZE], int size) {
 	return false;
 }
 
+bool moveLeft(int board[MAX_SIZE][MAX_SIZE], const int size, int& score) {
+	bool moved = false;
 
-bool moveLeft(int board[MAX_SIZE][MAX_SIZE], int size, int& score) {
-	
+	for (int i = 0; i < size; i++) {
+		int temp[MAX_SIZE] = { 0 };
+		int index = 0;
+
+		for (int j = 0; j < size; j++) {
+			if (board[i][j] != 0) {
+				temp[index++] = board[i][j];
+			}
+		}
+
+		for (int j = 0; j < index - 1; j++) {
+			if (temp[j] == temp[j + 1]) {
+				temp[j] *= 2;
+				score += temp[j];
+				temp[j + 1] = 0;
+				moved = true;
+			}
+		}
+
+		int finalRow[MAX_SIZE] = { 0 };
+		index = 0;
+		for (int j = 0; j < size; j++) {
+			if (temp[j] != 0) {
+				finalRow[index++] = temp[j];
+			}
+		}
+
+		for (int j = 0; j < size; j++) {
+			if (board[i][j] != finalRow[j]) {
+				moved = true;
+			}
+			board[i][j] = finalRow[j];
+		}
+	}
+
+	return moved;
 }
-bool moveRight(int board[MAX_SIZE][MAX_SIZE], int size, int& score) {
 
+bool moveRight(int board[MAX_SIZE][MAX_SIZE], const int size, int& score) {
+	bool moved = false;
+
+	for (int i = 0; i < size; i++) {
+		int temp[MAX_SIZE] = { 0 };
+		int index = size - 1;
+
+		for (int j = size - 1; j >= 0; j--) {
+			if (board[i][j] != 0) {
+				temp[index--] = board[i][j];
+			}
+		}
+
+		for (int j = size - 1; j > 0; j--) {
+			if (temp[j] == temp[j - 1] && temp[j] != 0) {
+				temp[j] *= 2;
+				score += temp[j];
+				temp[j - 1] = 0;
+				moved = true;
+			}
+		}
+
+		int finalRow[MAX_SIZE] = { 0 };
+		index = size - 1;
+		for (int j = size - 1; j >= 0; j--) {
+			if (temp[j] != 0) {
+				finalRow[index--] = temp[j];
+			}
+		}
+
+		for (int j = 0; j < size; j++) {
+			if (board[i][j] != finalRow[j]) {
+				moved = true;
+			}
+			board[i][j] = finalRow[j];
+		}
+	}
+
+	return moved;
 }
-bool moveUp(int board[MAX_SIZE][MAX_SIZE], int size, int& score) {
 
+bool moveUp(int board[MAX_SIZE][MAX_SIZE], const int size, int& score) {
+	bool moved = false;
+
+	for (int col = 0; col < size; col++) {
+		int temp[MAX_SIZE] = { 0 };
+		int index = 0;
+
+		for (int row = 0; row < size; row++) {
+			if (board[row][col] != 0) {
+				temp[index++] = board[row][col];
+			}
+		}
+
+		for (int i = 0; i < index - 1; i++) {
+			if (temp[i] == temp[i + 1]) {
+				temp[i] *= 2;
+				score += temp[i];
+				temp[i + 1] = 0;
+				moved = true;
+			}
+		}
+
+		int finalCol[MAX_SIZE] = { 0 };
+		index = 0;
+		for (int i = 0; i < size; i++) {
+			if (temp[i] != 0) {
+				finalCol[index++] = temp[i];
+			}
+		}
+
+		for (int row = 0; row < size; row++) {
+			if (board[row][col] != finalCol[row]) {
+				moved = true;
+			}
+			board[row][col] = finalCol[row];
+		}
+	}
+
+	return moved;
 }
-bool moveDown(int board[MAX_SIZE][MAX_SIZE], int size, int& score) {
 
+bool moveDown(int board[MAX_SIZE][MAX_SIZE], const int size, int& score) {
+	bool moved = false;
+
+	for (int col = 0; col < size; col++) {
+		int temp[MAX_SIZE] = { 0 };
+		int index = size - 1;
+
+		for (int row = size - 1; row >= 0; row--) {
+			if (board[row][col] != 0) {
+				temp[index--] = board[row][col];
+			}
+		}
+
+		for (int i = size - 1; i > 0; i--) {
+			if (temp[i] == temp[i - 1] && temp[i] != 0) {
+				temp[i] *= 2;
+				score += temp[i];
+				temp[i - 1] = 0;
+				moved = true;
+			}
+		}
+
+		int finalCol[MAX_SIZE] = { 0 };
+		index = size - 1;
+		for (int i = size - 1; i >= 0; i--) {
+			if (temp[i] != 0) {
+				finalCol[index--] = temp[i];
+			}
+		}
+
+		for (int row = 0; row < size; row++) {
+			if (board[row][col] != finalCol[row]) {
+				moved = true;
+			}
+			board[row][col] = finalCol[row];
+		}
+	}
+
+	return moved;
 }
 
-void updateLeaderboard(const char name[], int score, int size) {
+void updateLeaderboard(const char name[], const int score, const int size) {
 	char filename[50];
-	// buildLeaderboardFilename(filename, size); TODO...
+	buildLeaderboardFilename(filename, size);
 
 	char names[LEADERBOARD_SIZE][MAX_NAME];
 	int scores[LEADERBOARD_SIZE];
@@ -105,11 +302,11 @@ void updateLeaderboard(const char name[], int score, int size) {
 		}
 	}
 
-	if (pos >= LEADERBOARD_SIZE) {
+	if (pos >= LEADERBOARD_SIZE && count == LEADERBOARD_SIZE) {
 		return;
 	}
 
-	for (int i = LEADERBOARD_SIZE - 1; i > pos; i--) {
+	for (int i = count; i > pos; i--) {
 		myStrcpy(names[i], names[i - 1]);
 		scores[i] = scores[i - 1];
 	}
@@ -125,6 +322,7 @@ void updateLeaderboard(const char name[], int score, int size) {
 	for (int i = 0; i < count; i++) {
 		outFile << names[i] << " " << scores[i] << "\n";
 	}
+	outFile.close();
 }
 
 void showLeaderboard() {
@@ -133,7 +331,7 @@ void showLeaderboard() {
 	cin >> size;
 
 	char filename[50];
-	// buildLeaderboardFilename(filename, size); TODO...
+	buildLeaderboardFilename(filename, size);
 
 	ifstream file(filename);
 	if (!file.is_open()) {
@@ -148,6 +346,7 @@ void showLeaderboard() {
 	while (file >> name >> score) {
 		cout << name << " - " << score << "\n";
 	}
+	cout << "\n";
 	file.close();
 }
 
@@ -183,15 +382,14 @@ void startGame() {
 		else if (command == 'w') moved = moveUp(board, size, score);
 		else if (command == 's') moved = moveDown(board, size, score);
 
-		// TODO: 2048 hit logic (optional?)
-
 		if (moved) {
+			if (score >= 30) break;
 			addRandomTile(board, size);
 		}
 	}
 
 	printBoard(board, size, score);
-	cout << "\nGame Over!\n";
+	cout << "\nGame Over!\n\n";
 	updateLeaderboard(name, score, size);
 }
 
